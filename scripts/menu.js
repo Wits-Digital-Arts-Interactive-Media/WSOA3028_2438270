9a6865f5b906621030d69ce161a0a5ce7eb3aea4
BradSimbizi <bradsimbizi05092@gmail.com>
2024-04-19 11:40:41 +0200

    A few changes
    changed the blog structure and junk like that

---
+++ scripts/menu.js
@@ -0,0 +1,26 @@
+const menuItems= [
+    {name:"Home", href:"index.html"},
+    {name:"Blog Posts", href:"blogs/index.html"},
+    {name:"Essays", href:"essays.html"},
+    {name:"Portfolio", href:"portolio.html"},
+    {name:"Design Section", href:"design section.html"},
+]
+
+export function initialise(currentPage){
+    const nav=document.querySelector("header > nav")
+    const ul=document.createElement("ul")
+    const li=document.createElement("li")
+    for(let menuItem of menuItems){
+        const li=document.createElement("li")
+        if(currentPage !=menuItem.name){
+            const a=document.createElement("a")
+            a.innerText=menuItem.name
+            a.setAttribute("href", menuItem.href)
+            li.appendChild(a)
+        } else {li.innerText=menuItem.name}
+        ul.appendChild(li)
+    }
+    nav.appendChild(ul)
+}
+
+//initialise()
